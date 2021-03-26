adc66d08c198799d4238cb247781f15cb2df8e4a
Lionel Townsend <townsendlionel@gmail.com>
2021-03-26 18:46:18 -0500

    moved layout.hb to partialfolder and adjusted view engine references to match

--- src/travel-planner/app.js
+++ src/travel-planner/app.js
@@ -41,7 +41,7 @@ app.engine(
     helpers: multihelpers,
     partialsDir: ["views/partials"],
     extname: ".handlebars",
-    layoutsDir: "views",
+    layoutsDir: "views/partials",
     defaultLayout: "layout"
   })
 );
@@ -54,7 +54,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use(require('./controllers/'));
 
 // catch 404 and forward to error handler
