b7859079965c1d9adfb8c7500b8f32eb3e4e3b22
Halleys-Comet <jhalley230@gmail.com>
2021-03-27 12:20:23 -0500

    create user routes

--- src/travel-planner/controllers/api/user-routes.js
+++ src/travel-planner/controllers/api/user-routes.js
@@ -1,4 +1,93 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { User } = require('../../models');
 
 
+router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+
+}) 
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+        id: req.params.id
+        }
+    }) 
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req, res)=> {
+// expects {username: 'Johno', email: 'johno@gmail.com', password: 'password1234'}
+
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+
+    // expects {username: 'Johno', email: 'johno@gmail.com', password: 'password1234'}
+    User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- src/travel-planner/controllers/index.js
+++ src/travel-planner/controllers/index.js
@@ -7,4 +7,9 @@ const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+router.use((req, res) => {
+    res.status(404).end();
+  });
+  
+
 module.exports = router;
