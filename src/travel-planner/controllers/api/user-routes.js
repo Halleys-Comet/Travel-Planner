ad8efa25e07141dda548075d9346243b00c82333
Halleys-Comet <jhalley230@gmail.com>
2021-03-30 17:01:51 -0500

    comment out logout

--- src/travel-planner/controllers/api/user-routes.js
+++ src/travel-planner/controllers/api/user-routes.js
@@ -128,16 +128,16 @@ router.delete('/:id', (req, res) => {
         });
 });
 
-router.post('/logout',  (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  }
-  else {
-    res.status(404).end();
-  }
+// router.post('/logout',  (req, res) => {
+//   if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   }
+//   else {
+//     res.status(404).end();
+//   }
 
-});
+// });
 
 module.exports = router;
